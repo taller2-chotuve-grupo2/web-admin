2e15396176aa70a3896f97e301f542b12101d4ee
Juan Pablo Donato <judonato@fi.uba.ar>
2020-04-22 20:07:32 -0300

    Ultra Basic Login structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import "./index.css";
+import {Button, Row, Container, Col, FormControl, InputGroup} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-    return(
-        <div className="App">
-            <h1>Bienvenidos a Chotuve</h1>
-        </div>
-    );
+
+export default function Example() {
+  // Declare a new state variable, which we'll call "count"
+  //const [count, setCount] = useState(0);
+    const [usuario, setUsuario] = useState("");
+    const [password, setPassword] = useState("");
+
+    function limpiarCampos(){
+        setUsuario("");
+        setPassword("");
+    }
+
+  return (
+    <div> 
+    <Container fluid="sm">
+        <Row className="d-flex justify-content-center align-items-center">
+            <Col className="p-4 col-4">
+
+                
+
+                <h1>Inicio de sesión</h1>
+                
+                {/* Input group Usuario */}
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Usuario</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                    id="txtUsuario"
+                    value={usuario}
+                    placeholder="Usuario"
+                    aria-label="Usuario"
+                    aria-describedby="basic-addon1"
+                    onChange= {e => setUsuario(e.target.value)}
+                    />
+                </InputGroup>
+
+                {/* Input Group Contraseña */}
+                <InputGroup className="mb-3" controlId="asd">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Contraseña</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                    id="txtPassword"
+                    value={password}
+                    type="password"
+                    placeholder="Constraseña"
+                    aria-label="Contraseña"
+                    aria-describedby="basic-addon1"
+                    onChange= {e => setPassword(e.target.value)}
+                    />
+                </InputGroup>
+
+                {/* Boton Log In */}
+                <Button onClick={limpiarCampos}>Login</Button>
+
+
+
+            </Col>
+        </Row>
+    </Container>
+    </div>
+  );
 }
 
-export default App;
+export class Ejemplo extends React.Component{
+    render(){
+        return(
+            <div className="example">
+                <Example />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import Ejemplo from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Ejemplo />
   </React.StrictMode>,
   document.getElementById('root')
 );
