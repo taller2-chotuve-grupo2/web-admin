c757330825294e0af8f574aa39094e7d3bb78c73
IgVelasco <IgVelasco@users.noreply.github.com>
2020-04-26 21:54:42 +0000

    deploy: 1ba28a54b866996618eb084972e2fbb42feb1f07

---
+++ precache-manifest.8c4203cd23390a72ec361e2d82d2f6b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be8638d19a2bc289bb1b8b9165851b0",
+    "url": "/web-admin/index.html"
+  },
+  {
+    "revision": "73ad9ab946c7346b8c53",
+    "url": "/web-admin/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b4d6d9bccee571b89ac7",
+    "url": "/web-admin/static/css/main.56cc78b9.chunk.css"
+  },
+  {
+    "revision": "73ad9ab946c7346b8c53",
+    "url": "/web-admin/static/js/2.8ffa4dda.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-admin/static/js/2.8ffa4dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d6d9bccee571b89ac7",
+    "url": "/web-admin/static/js/main.01ce1548.chunk.js"
+  },
+  {
+    "revision": "f2f3bbc67346e5ccc8ac",
+    "url": "/web-admin/static/js/runtime-main.42cccff3.js"
+  }
+]);
