f00319bfd5b23f92a57a88960c62d50d615ef89d
juan <juandambra@programming4design.com>
2020-07-05 01:15:41 -0300

    added thumbnail field , material ui user table

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,14 +1,53 @@
-import React from 'react'
+import  React from 'react'
+import { forwardRef } from 'react'
+
 import '../index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Redirect, useHistory } from 'react-router-dom'
 import { HomeHeader, HomeRect } from './HomePage'
-import JsonTable from 'ts-react-json-table'
+// import JsonTable from 'ts-react-json-table'
 import axios from 'axios'
-import styles from '../styles/usersTable.css'
+// import styles from '../styles/usersTable.css'
+import MaterialTable from 'material-table'
+import Avatar from '@material-ui/core/Avatar'
+import AddBox from '@material-ui/icons/AddBox'
+import ArrowDownward from '@material-ui/icons/ArrowDownward'
+import Check from '@material-ui/icons/Check'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import Clear from '@material-ui/icons/Clear'
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
+import Edit from '@material-ui/icons/Edit'
+import FilterList from '@material-ui/icons/FilterList'
+import FirstPage from '@material-ui/icons/FirstPage'
+import LastPage from '@material-ui/icons/LastPage'
+import Remove from '@material-ui/icons/Remove'
+import SaveAlt from '@material-ui/icons/SaveAlt'
+import Search from '@material-ui/icons/Search'
+import ViewColumn from '@material-ui/icons/ViewColumn'
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+}
 
 const baseUrl = process.env.REACT_APP_AUTH_BASE_URL
-const usersApi = `${baseUrl}/users/`;
+const usersApi = `${baseUrl}/users/`
 
 export default function UsersPage () {
   const history = useHistory()
@@ -47,7 +86,7 @@ class UsersBody extends React.Component {
   componentDidMount () {
     const axiosConfig = {
       headers: {
-        'Authorization': localStorage.getItem('token'),
+        Authorization: localStorage.getItem('token'),
         'Content-Type': 'application/json'
       }
     }
@@ -55,10 +94,36 @@ class UsersBody extends React.Component {
   }
 
   render () {
-    return <JsonTable
-      rows={this.state.usersData} columns={this.state.columns} class={styles}
-      settings={{ noRowsMessage: 'Loading or no permissions' }}
-           />
+    return (
+      <div style={{ maxWidth: '100%' }}>
+        <MaterialTable
+          icons={tableIcons}
+          columns={[
+            { 
+              field: 'thumbnail', 
+              title: 'thumbnail', 
+              render: rowData => <Avatar alt="Remy Sharp" src={rowData.thumbnail} />
+            },
+            { field: 'username', title: 'Username' },
+            { field: 'password', title: 'Encripted Password' },
+            { field: 'email', title: 'Email' },
+            { field: 'first_name', title: 'first_name' },
+            { field: 'last_name', title: 'last_name' },
+            { field: 'phone', title: 'phone' },
+            { field: 'createdAt', title: 'Creation Date' },
+            { field: 'updatedAt', title: 'Last Update' }
+
+            // { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+          ]}
+          data={this.state.usersData}
+          title='Users'
+        />
+      </div>
+      // <JsonTable
+      //   rows={this.state.usersData} columns={this.state.columns} class={styles}
+      //   settings={{ noRowsMessage: 'Loading or no permissions' }}
+      //        />
+    )
   }
 
   getUsersFromAuth (axiosConfig) {
